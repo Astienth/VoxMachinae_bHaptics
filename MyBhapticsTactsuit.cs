f3fa35450f0b1344d9a12c12138346b850d3be90
Astienth <astienth@gmail.com>
2022-03-28 15:49:27 +0200

    initial commit

---
+++ MyBhapticsTactsuit.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using MelonLoader;
+
+namespace MyBhapticsTactsuit
+{
+    public class TactsuitVR
+    {
+        public bool suitDisabled = true;
+        public bool systemInitialized = false;
+        // dictionary of all feedback patterns found in the bHaptics directory
+        public Dictionary<String, FileInfo> FeedbackMap = new Dictionary<String, FileInfo>();
+
+        private static bHaptics.RotationOption defaultRotationOption = new bHaptics.RotationOption(0.0f, 0.0f);
+
+        public TactsuitVR()
+        {
+            LOG("Initializing suit");
+            if (!bHaptics.WasError)
+            {
+                suitDisabled = false;
+            }
+            RegisterAllTactFiles();
+        }
+
+        public void LOG(string logStr)
+        {
+#pragma warning disable CS0618 // remove warning that the logger is deprecated
+            MelonLogger.Msg(logStr);
+#pragma warning restore CS0618
+        }
+
+        void RegisterAllTactFiles()
+        {
+            // Get location of the compiled assembly and search through "bHaptics" directory and contained patterns
+            string configPath = Directory.GetCurrentDirectory() + "\\Mods\\bHaptics";
+            DirectoryInfo d = new DirectoryInfo(configPath);
+            FileInfo[] Files = d.GetFiles("*.tact", SearchOption.AllDirectories);
+            for (int i = 0; i < Files.Length; i++)
+            {
+                string filename = Files[i].Name;
+                string fullName = Files[i].FullName;
+                string prefix = Path.GetFileNameWithoutExtension(filename);
+                // LOG("Trying to register: " + prefix + " " + fullName);
+                if (filename == "." || filename == "..")
+                    continue;
+                string tactFileStr = File.ReadAllText(fullName);
+                try
+                {
+                    bHaptics.RegisterFeedbackFromTactFile(prefix, tactFileStr);
+                    LOG("Pattern registered: " + prefix);
+                }
+                catch (Exception e) { LOG(e.ToString()); }
+
+                FeedbackMap.Add(prefix, Files[i]);
+            }
+            systemInitialized = true;
+        }
+
+        public void PlaybackHaptics(String key, float intensity = 1.0f, float duration = 1.0f)
+        {
+            if (FeedbackMap.ContainsKey(key))
+            {
+                bHaptics.ScaleOption scaleOption = new bHaptics.ScaleOption(intensity, duration);
+                bHaptics.SubmitRegistered(key, key, scaleOption, defaultRotationOption);
+            }
+            else
+            {
+                LOG("Feedback not registered: " + key);
+            }
+        }
+
+        public void PlayBackHit(String key, float xzAngle, float yShift)
+        {
+            // two parameters can be given to the pattern to move it on the vest:
+            // 1. An angle in degrees [0, 360] to turn the pattern to the left
+            // 2. A shift [-0.5, 0.5] in y-direction (up and down) to move it up or down
+            bHaptics.ScaleOption scaleOption = new bHaptics.ScaleOption(1f, 1f);
+            bHaptics.RotationOption rotationOption = new bHaptics.RotationOption(xzAngle, yShift);
+            bHaptics.SubmitRegistered(key, key, scaleOption, rotationOption);
+        }
+
+        public bool IsPlaying(String effect)
+        {
+            return bHaptics.IsPlaying(effect);
+        }
+
+        public void StopHapticFeedback(String effect)
+        {
+            bHaptics.TurnOff(effect);
+        }
+
+        public void StopAllHapticFeedback()
+        {
+            foreach (String key in FeedbackMap.Keys)
+            {
+                bHaptics.TurnOff(key);
+            }
+        }
+    }
+}
---
+++ VoxMachinae_bHaptics.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using MelonLoader;
+using HarmonyLib;
+using MyBhapticsTactsuit;
+using InControl;
+
+namespace VoxMachinae_Bhaptics
+{
+    public class VoxMachinae_Bhaptics : MelonMod
+    {
+        public static TactsuitVR tactsuitVr;
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+            tactsuitVr = new TactsuitVR();
+            tactsuitVr.PlaybackHaptics("HeartBeat");
+        }
+        
+        [HarmonyPatch(typeof(InputDevice), "Vibrate")]
+        public class bhaptics_TriggerUsed
+        {
+            [HarmonyPostfix]
+            public static void Postfix(InputDevice __instance)
+            {
+                if (tactsuitVr.suitDisabled) return;
+
+                tactsuitVr.LOG("VIBRATE ");
+            }
+        }
+    }
+}
